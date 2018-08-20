340cbc1c3575063aef4cae2e394222c74985e4c8
Odair José Gambert Nossa <odairjgn@gmail.com>
2018-08-20 10:41:39 -0300

    Didi

---
+++ WFZ_Engine/Extentions/CompressExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace WFZ_Engine.Extentions
+{
+    public static class CompressExtentions
+    {
+        public static async void Teste()
+        {
+            ZipArchive zip = ZipFile.Open("zip", ZipArchiveMode.Update, Encoding.UTF8);
+            var strm = zip.Entries[0].Open();
+            await strm.WriteAsync(new byte[] {1, 2, 3}, 0, 3);
+        }
+    }
+}
